a44bf509f32c5761cf7425564765fb4559dd1b26
447064 <anitha.edith@cognizant.com>
2018-03-14 12:22:12 +0530

    Code Reformatted

--- src/test/java/io/swagger/UnitTestBootApplicationTest.java
+++ src/test/java/io/swagger/UnitTestBootApplicationTest.java
@@ -1,11 +1,11 @@
-package io.swagger.api;
+package io.swagger;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith( SpringRunner.class )
 @SpringBootTest
 public class UnitTestBootApplicationTest {
     @Test
